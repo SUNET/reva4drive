9ae9742e2133e1baebe66fe97a4266cd4948613c
Micke Nordin <kano@sunet.se>
2022-12-06 19:45:56 +0100

    Fix /var/tmp/reva/ocm-invites.json

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mkdir /var/tmp/reva/
+touch /var/tmp/reva/ocm-invites.json
 /usr/local/bin/revad -c /etc/revad/revad.toml -p /run/revad/revad.pid &> /proc/1/fd/1 &
 apachectl -D FOREGROUND
